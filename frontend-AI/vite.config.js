f467b859a3266e6cdf74ba00fc16824e684a19cc
m4Liiii <malekhiw04@gmail.com>
2024-12-13 20:23:14 +0300

    lets go again

--- frontend-AI/vite.config.js
+++ frontend-AI/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
-import vue from "/vitejs/plugin-vue";
+import vue from "@vitejs/plugin-vue";
 
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/',
   server: {
     port: 3000,
   },
