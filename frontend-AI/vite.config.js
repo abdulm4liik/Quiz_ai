4007aa0a687b08df27871bf529e4aaf93f76ad5c
m4Liiii <malekhiw04@gmail.com>
2024-12-13 19:53:54 +0300

    go

--- frontend-AI/vite.config.js
+++ frontend-AI/vite.config.js
@@ -1,17 +1,14 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: '/frontend-AI/',
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": "/src", 
     },
   },
   server: {
     port: 3000,
   },
 });
-
